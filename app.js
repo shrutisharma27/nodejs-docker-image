4cfd6b560159def48379f7aa0e15abaacf1d28ff
Shruti sharma <krati465@gmail.com>
2021-08-13 10:39:17 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => res.send('Hello World.!!'))
+app.get('/', (req, res) => res.send('Hello World 2.!!'))
 app.listen(8080, () => console.log('Server ready'))
